5cb44b0acabf74f9e4b93d51c92340f036294ee0
Лиана Марданова <lianamardanova@MacBook-Air-Liana.local>
2023-07-12 23:36:59 +0300

    Added carousel component

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+	import StudioPhoto1 from '$lib/studio1.jpeg';
+</script>
+
---
+++ src/routes/components/Carousel.svelte
@@ -0,0 +1,46 @@
+<script>
+    import StudioPhoto1 from '$lib/studio1.jpeg'
+    import StudioPhoto2 from '$lib/studio2.jpeg'
+    import StudioPhoto3 from '$lib/studio3.jpeg'
+</script>
+
+<style>
+    .carousel {
+        
+        max-height: auto; 
+        max-width: 400px;/* Adjust the max-height value as needed */
+        overflow: hidden;
+    }
+
+</style>
+
+<div class="carousel w-full">
+    <div id="slide1" class="carousel-item relative w-full">
+        <img src={StudioPhoto1} alt="Art studio" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" class="btn btn-circle">❮</a>
+            <a href="#slide2" class="btn btn-circle">❯</a>
+        </div>
+    </div>
+    <div id="slide2" class="carousel-item relative w-full">
+        <img src={StudioPhoto2} alt="Art studio" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" class="btn btn-circle">❮</a>
+            <a href="#slide3" class="btn btn-circle">❯</a>
+        </div>
+    </div>
+    <div id="slide3" class="carousel-item relative w-full">
+        <img src={StudioPhoto3} alt="Art studio" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" class="btn btn-circle">❮</a>
+            <a href="#slide4" class="btn btn-circle">❯</a>
+        </div>
+    </div>
+    <div id="slide4" class="carousel-item relative w-full">
+        <img src={StudioPhoto1} alt="Art studio" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" class="btn btn-circle">❮</a>
+            <a href="#slide1" class="btn btn-circle">❯</a>
+        </div>
+    </div>
+</div>
