b0f3145055ee36904bcb9427dec4076b3179e55f
Лиана Марданова <lianamardanova@MacBook-Air-Liana.local>
2023-07-12 23:12:16 +0300

    Added contact form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
 	import '../app.postcss';
 	import Header from './components/Header.svelte';
-	import Footer from'./components/Footer.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
-<Header/>
-<slot />
-<Footer/>
+<body>
+	<Header />
+	<slot />
+	<Footer />
+</body>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;300;400&family=Playfair+Display:wght@400;600;700&display=swap');
+	body {
+		font-family: 'IBM Plex Mono', monospace;
+	}
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+    let name = '';
+    let email = '';
+    let country = '';
+    let message = '';
+  
+    function handleSubmit() {
+      // ТODO: Handle form submission here
+      console.log('Form submitted');
+    }
+  </script>
+  
+  <section class="flex flex-col items-center justify-center h-screen">
+    <div class="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-lg border border-gray-300">
+      <!-- <h2 class="text-3xl font-semibold mb-6">Contact</h2> -->
+      <p class="mt-4 text-center">If you have any questions, please feel free to send a message, or you can contact me</p>
+      <p class="text-center mb-4">Thank you, Hiroshi</p>
+  
+      <form on:submit={handleSubmit} class="space-y-4">
+        <div>
+          <label class="block text-sm font-medium">
+            Name:
+            <input type="text" bind:value={name} class="form-input mt-1 w-full px-3 py-2 border border-gray-300 rounded-md" required />
+          </label>
+        </div>
+  
+        <div>
+          <label class="block text-sm font-medium">
+            Email:
+            <input type="email" bind:value={email} class="form-input mt-1 w-full px-3 py-2 border border-gray-300 rounded-md" required />
+          </label>
+        </div>
+  
+        <div>
+          <label class="block text-sm font-medium">
+            Country:
+            <input type="text" bind:value={country} class="form-input mt-1 w-full px-3 py-2 border border-gray-300 rounded-md" required />
+          </label>
+        </div>
+  
+        <div>
+          <label class="block text-sm font-medium">
+            Message:
+            <textarea bind:value={message} class="form-textarea mt-1 w-full px-3 py-2 border border-gray-300 rounded-md" rows="4" required></textarea>
+          </label>
+        </div>
+  
+        <div class="text-center">
+          <button type="submit" class="btn px-4 py-2">Submit</button>
+        </div>
+      </form>
+    </div>
+  </section>
+
+  
+  
