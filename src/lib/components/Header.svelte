aa9f025aa8e70f550260df6a540337a5b1dd8473
Лиана Марданова <lianamardanova@MacBook-Air-Liana.local>
2023-07-18 10:00:20 +0300

    Updated link in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,16 +4,18 @@
 
 <div class="navbar bg-base-200">
 	<div class="flex-1">
-	  <div class="flex items-center m-8 ml-14">
-	    <img src={LogoImage} alt="Logo" class="h-6 w-6 mr-4">
-	    <a href="/" class="text-xs">HIROSHI MATSUMOTO</a>
-	  </div>
+		<div class="flex items-center m-8 ml-14">
+			<a href="/">
+				<img src={LogoImage} alt="Logo" class="h-6 w-6 mr-4" />
+				<p class="text-xs">HIROSHI MATSUMOTO</p></a
+			>
+		</div>
 	</div>
 	<div class="flex-none">
-	  <ul class="menu menu-horizontal px-1">
-		<li class="text-xs" ><a href="/paintings">PAINTINGS</a></li>
-		<li class="text-xs" ><a href="/about">ABOUT</a></li>
-		<li class="text-xs"><a href="/contact">CONTACT</a></li>
-	  </ul>
+		<ul class="menu menu-horizontal px-1">
+			<li class="text-xs"><a href="/paintings">PAINTINGS</a></li>
+			<li class="text-xs"><a href="/about">ABOUT</a></li>
+			<li class="text-xs"><a href="/contact">CONTACT</a></li>
+		</ul>
 	</div>
-  </div>
+</div>
