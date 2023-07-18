d5c4056918e7ee7c74dace36663598705d2526a3
Лиана Марданова <lianamardanova@MacBook-Air-Liana.local>
2023-07-18 12:03:56 +0300

    Fixed centering in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,6 @@
 
 <div class="flex flex-col items-center justify-center w-4/5 max-w-xl mx-auto mt-16 mb-16">
 	<img class="w-full mb-4" src={imgTest} alt="Today's painting" />
-	<div class="text-xl font-bold mb-4">Original Abstract Oil Paintings by Hiroshi Matsumoto</div>
+	<div class="text-xl font-bold mb-4 text-center">Original Abstract Oil Paintings by Hiroshi Matsumoto</div>
 	<button class="btn"><a href="./paintings">Explore</a></button>
 </div>
