b93b98e0a10b61f19919e2c974b5fc251bdb9adc
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-24 01:12:17 +0300

    w3 update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,17 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<body>
+<div class="container">
 	<Header />
 	<main>
 		<slot />
 	</main>
 	<Footer />
-</body>
+</div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;300;400&family=Karla:wght@200;300;400;600;700&family=Playfair+Display:wght@400;600;700&display=swap');
-	body {
+	.container {
 		/* font-family: 'IBM Plex Mono', monospace; */
 		font-family: 'Karla', sans-serif;
 		/* font-family: 'Playfair Display', serif; */
