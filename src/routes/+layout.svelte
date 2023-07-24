9adfd77b6e8a7cac1828b784e900026f525b4daf
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-24 11:01:13 +0300

    moved font to external css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,10 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="container">
+<body>
 	<Header />
 	<main>
 		<slot />
 	</main>
 	<Footer />
-</div>
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;300;400&family=Karla:wght@200;300;400;600;700&family=Playfair+Display:wght@400;600;700&display=swap');
-	.container {
-		/* font-family: 'IBM Plex Mono', monospace; */
-		font-family: 'Karla', sans-serif;
-		/* font-family: 'Playfair Display', serif; */
-	}
-
-	/* не знаю как подключить шрифт если убрать body */
-</style>
+</body>
