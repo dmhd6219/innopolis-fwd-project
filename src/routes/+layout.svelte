689bbd98119c78e48756adb9f740014d3b9a2af5
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-24 11:02:45 +0300

    moved font to external css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
-	import '../app.postcss';
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+    import '../app.postcss';
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+    import '../app.css';
 </script>
 
 <body>
-	<Header />
-	<main>
-		<slot />
-	</main>
-	<Footer />
+<Header/>
+<main>
+    <slot/>
+</main>
+<Footer/>
 </body>
